64304724bcba156902e058da62665254741bfd4f
HappyNZ95 <htewini@gmail.com>
2025-01-14 21:44:26 +1300

    started working on scenes class

---
+++ src/scenes/scene.py
@@ -0,0 +1,8 @@
+class Scene:
+    def __init__(self, name, brightness, colours):
+        self.name = name
+        self.brightness = brightness
+        self.colours = colours
+
+#to be continued....
+ibiza = Scene()
