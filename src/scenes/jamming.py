2a398c7132fb94d2ba83b2d2af8247740894ae03
HappyNZ95 <htewini@gmail.com>
2025-01-17 20:21:02 +1300

    added 'jamming' scene

---
+++ src/scenes/jamming.py
@@ -0,0 +1,41 @@
+from model.scene import Scene
+
+colours = [
+{
+    "xy": {
+                        "x": 0.5058,
+                        "y": 0.4151
+                    }
+    },
+
+    {
+        "xy": {
+                            "x": 0.6362,
+                            "y": 0.2999
+                        }
+    },
+
+    {
+        "xy": {
+                            "x": 0.6365,
+                            "y": 0.2998
+                        }
+    },
+
+    {
+        "xy": {
+                            "x": 0.5685,
+                            "y": 0.4007
+                        }
+    },
+
+    {
+        "xy": {
+                            "x": 0.5058,
+                            "y": 0.4151
+                        }
+    }
+]
+
+default_brightness = 63
+jamming = Scene("jamming", colours, default_brightness)
--- src/scenes/scenes.py
+++ src/scenes/scenes.py
@@ -1,7 +1,7 @@
 from scenes.ibiza import ibiza
 from scenes.nebula import nebula
+from scenes.jamming import jamming
 
 
 
-
-scenes = [ibiza, nebula]
+scenes = [ibiza, nebula, jamming]
